6b1aff9fa1881418d20a21b1c4f8cd0fda1d4180
lidiamoryc <lidiamoryc@onet.pl>
2025-01-15 13:05:59 +0100

    backend rest api - all the cunctions implemented

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js")
+const app = express();
 
-const app = express()
-
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
+// routes
+app.use("/api/products", productRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello from node api server');
 });
 
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
 
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
+// app.get('/api/products', async (req, res) => {
+//     try {
+//         const products = await Product.find({});
+//         res.status(200).json(products);
 
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
+//     } catch (error) {
+//         res.status(500).json({message: error.message});
+//     }
+// });
+
+
+// app.get('/api/products/:id', async (req, res) => {
+//     try {
+//         const {id} = req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product);
+
+//     } catch (error) {
+//         res.status(500).json({message: error.message});
+//     }
+// });
 
 
 app.post('/api/products', async (req, res) => {
@@ -47,6 +53,46 @@ app.post('/api/products', async (req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+
+// update
+app.put('/api/products/:id', async (req, res) => {
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({message: "product not found"})
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+//delete 
+app.delete('/api/products/:id', async (req, res) => {
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message: "product not found"})
+        }
+
+        res.status(200).json("product deleted successfully");
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 
 mongoose.connect("mongodb+srv://admin:admin@backenddb.dxynh.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
