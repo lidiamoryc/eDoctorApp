869b9e5acfb42a5a83d23a48dd095203bcc4f9d4
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2025-01-16 18:36:14 +0100

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
     absenceApiUrl: 'http://localhost:3000/absence',
     firebaseConfig: {
     apiKey: "AIzaSyAe7yDuX-g0CgloQWfCbiPfm0A4A-quJxk",
-    authDomain: "edoctorapp-d1d18.firebaseapp.com",
-    projectId: "edoctorapp-d1d18",
-    storageBucket: "edoctorapp-d1d18.firebasestorage.app",
+    authDomain: "edoctorapp-f0008.firebaseapp.com",
+    projectId: "edoctorapp-f0008",
+    storageBucket: "edoctorapp-f0008.appspot.com",
     messagingSenderId: "989131274547",
     appId: "1:989131274547:web:44d655db1da6de3a9f6646",
     measurementId: "G-JRNLYBG2MW"
